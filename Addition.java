e792848ff5da4c3743818f5100da800654d74c23
shruthigorantala <134036339+shruthigorantala@users.noreply.github.com>
2023-05-19 13:39:32 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20, b=5,c;
+		c=a+b;
+		System.out.println("Addition :" +c);
+
+	}
+
+}
